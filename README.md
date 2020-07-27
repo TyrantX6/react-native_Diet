ae843cce855cd512190d91ae81e6e14fcafdacbb
TyrantX6 <thomas.pottier60@gmail.com>
2020-07-27 11:14:12 +0200

    Style appliqué, Touchable opacity ajouté.Ajout component ListItem.js

--- README.md
+++ README.md
@@ -2,4 +2,5 @@ README
 
 - création projet avec npx react init ThomasDiet
 - ajout package Async Storage
-
+- création dossier app : App.js déplacé dedans, création dossier components
+- nettoyage des fichiers de base.
--- app/App.js
+++ app/App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,6 +14,9 @@ import {
   View,
   Text,
   Button,
+  TouchableHighlight,
+  TouchableOpacity,
+
 
 } from 'react-native';
 
@@ -21,15 +24,65 @@ import {
 
 export default  App = () => {
 
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
+
   return (
     <SafeAreaView>
 
-      <Text>Nouvelle tâche :</Text>
+
+
+      <View style={styles.mealContainer}>
+        <View style={styles.mealTitle}>
+          <Text style={styles.mealTitleText}>Petit déjeuner</Text>
+          <TouchableOpacity
+
+            onPress={onPress}
+          >
+            <View>
+              <Text style={styles.mealTitlePlusButton}>+</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.countContainer}>
+          <Text>Count: {count}</Text>
+        </View>
+      </View>
+
+
+
 
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  mealContainer : {
+    alignContent : 'center',
+  },
+  mealTitle : {
+    flexDirection: 'row',
+    backgroundColor : '#3A8E3A',
+    paddingVertical : 6,
+    paddingHorizontal: 12,
+    width: '100%',
+    maxWidth : '90%',
+    justifyContent : 'space-between',
+    alignItems : 'center',
+    borderRadius: 20,
+    alignSelf: 'center'
+  },
+  mealTitleText : {
+    color:'#EFEFEF',
+    fontSize: 26,
+  },
+  mealTitlePlusButton : {
+    color:'#EFEFEF',
+    fontSize: 36,
+    backgroundColor: '#85C685',
+    borderRadius : 20,
+    paddingHorizontal: 16
+  },
+
 
 });
